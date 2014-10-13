bc33ed53cba826a7a25137929434bfd412cb8e41
Karim Samadi <karim.samadi.ks@gmail.com>
2014-10-13 18:32:11 -0400

    initial commit

---
+++ myMeProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myMeProject
+//
+//  Created by 18samadik on 10/13/14.
+//  Copyright (c) 2014 18samadik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myMeProject/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  myMeProject
+//
+//  Created by 18samadik on 10/13/14.
+//  Copyright (c) 2014 18samadik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
